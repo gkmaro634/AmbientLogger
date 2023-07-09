60fc57e2502549760f541a6d142b488ae85034d2
Kazuma Goto <oosotokara.kgoto@gmail.com>
2023-07-09 23:24:20 +0900

    センサ取得と表示更新を別スレッド処理に変更

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,6 @@
 
 WiFiClient client;
 
-hw_timer_t * timer0 = NULL;
-portMUX_TYPE timer0Mutex = portMUX_INITIALIZER_UNLOCKED;
-volatile bool isTimer0Ticked = false;
-const uint16_t timer0Prescaler = 80;// 80MHz / 80 = 1MHz
-const int esp32SystemClock = 80 * 1e6;
-const int timer0TickIntervalMilliSeconds = 30 * 1000;
-uint64_t timer0InterruptTick =  (double)esp32SystemClock / (double)timer0Prescaler * (double)timer0TickIntervalMilliSeconds / 1000.0;
-
 MyDateTime dt;
 
 // view model
@@ -31,10 +23,26 @@ Measurement ccpm("CO2", "[ppm]");
 SHT3X sht30;
 MHZ19C mhz19c;
 
-void IRAM_ATTR onTimer0Ticked(){
-	portENTER_CRITICAL_ISR(&timer0Mutex);
-	isTimer0Ticked = true;
-	portEXIT_CRITICAL_ISR(&timer0Mutex);
+void acquisitionTask(void* arg)
+{
+  Wire.begin();
+  mhz19c = MHZ19C(36);
+
+  while (1)
+  {
+    if(mhz19c.get() == 0)
+    {
+      ccpm.SetValue(mhz19c.ccpm);
+    }
+
+    if(sht30.get() == 0)
+    {
+      temperature.SetValue(sht30.cTemp);
+      humidity.SetValue(sht30.humidity);
+    }
+
+    vTaskDelay(100);
+  }
 }
 
 void setup()
@@ -46,17 +54,10 @@ void setup()
 	disableCore0WDT();
 	disableCore1WDT();
 
-  Wire.begin();
 
 	// M5.Lcd.sleep();
 	// M5.Lcd.setBrightness(0);
 	// Serial.println("Hello World!");
-
-  mhz19c = MHZ19C(36);
-
-	timer0 = timerBegin(0, timer0Prescaler, true);// 1us?
-	timerAttachInterrupt(timer0, &onTimer0Ticked, true);
-	timerAlarmWrite(timer0, timer0InterruptTick, true);
 
 	WiFi.begin(ssid, pass);
 	while( WiFi.status() != WL_CONNECTED)
@@ -74,67 +75,31 @@ void setup()
 	delay(1000);
 	M5.Lcd.clear();
 
-	timerAlarmEnable(timer0);
+  xTaskCreatePinnedToCore(acquisitionTask, "Task0", 4096, NULL, 1, NULL, 1);
 }
-
-bool isSetValue = false;
-bool toUpdateDisplay = false;
 
 void loop()
 {
-	portENTER_CRITICAL_ISR(&timer0Mutex);
-	if(isTimer0Ticked == true)
-  {
-		isSetValue = true;
-		isTimer0Ticked = false;
-	}
-	portEXIT_CRITICAL_ISR(&timer0Mutex);
-
-	if(isSetValue == true){
-		isSetValue = false;
-	}
-
-  if(mhz19c.get() != 0)
-  {
-    toUpdateDisplay = false;
-    ccpm.SetValue(0.0);
-  }
-  else
-  {
-    toUpdateDisplay = true;
-    ccpm.SetValue(mhz19c.ccpm);
-  }
-
-  if(sht30.get() == 0)
-  {
-    temperature.SetValue(sht30.cTemp);
-    humidity.SetValue(sht30.humidity);
-  }
-
   dt.GetLocalTime();
 
-  // display current values
-  if(toUpdateDisplay == true)
-  {
-    M5.Lcd.setCursor(0, 0);
-    // M5.Lcd.println("Failed to obtain time");
-    M5.Lcd.printf("%02d:%02d:%02d\r\n" ,dt.dt_hour ,dt.dt_min ,dt.dt_sec);
+  M5.Lcd.setCursor(0, 0);
 
-    M5.Lcd.printf("\r\n");
+  M5.Lcd.printf("%02d:%02d:%02d\r\n" ,dt.dt_hour ,dt.dt_min ,dt.dt_sec);
 
-    M5.Lcd.printf("%s:\r\n", ccpm.GetTitle());
-    M5.Lcd.printf("%4.2f %s\r\n", ccpm.GetValue(), ccpm.GetUnit());
-    M5.Lcd.printf("\r\n");
+  M5.Lcd.printf("\r\n");
 
-    M5.Lcd.printf("%s:\r\n", temperature.GetTitle());
-    M5.Lcd.printf("%2.1f %s\r\n", temperature.GetValue(), temperature.GetUnit());
-    M5.Lcd.printf("\r\n");
+  M5.Lcd.printf("%s:\r\n", ccpm.GetTitle());
+  M5.Lcd.printf("%4.2f %s\r\n", ccpm.GetValue(), ccpm.GetUnit());
+  M5.Lcd.printf("\r\n");
 
-    M5.Lcd.printf("%s:\r\n", humidity.GetTitle());
-    M5.Lcd.printf("%2.1f %s\r\n", humidity.GetValue(), humidity.GetUnit());
-  }
+  M5.Lcd.printf("%s:\r\n", temperature.GetTitle());
+  M5.Lcd.printf("%2.1f %s\r\n", temperature.GetValue(), temperature.GetUnit());
+  M5.Lcd.printf("\r\n");
 
-	delay(1);
+  M5.Lcd.printf("%s:\r\n", humidity.GetTitle());
+  M5.Lcd.printf("%2.1f %s\r\n", humidity.GetValue(), humidity.GetUnit());
+
+	vTaskDelay(200);
 }
 
 
